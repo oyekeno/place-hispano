b3deea1ad629da65f1f577f8a708332def548558
oyekeno <oyekeno@gmail.com>
2022-04-04 14:03:33 -0600

    updated img

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/Gerardo-Pastor/MexicoPlace/main/MexicoFTW_template.png";
+            i.src = "https://raw.githubusercontent.com/oyekeno/place-hispano/main/test.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
