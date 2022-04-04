bfa8381f837c055ca2ad722915c05671e358f269
oyekeno <oyekeno@gmail.com>
2022-04-04 14:07:41 -0600

    Updated

--- userscript.user.js
+++ userscript.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         r/Mexico Template
+// @name         Place Hispano
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the canvas!
-// @author       Gerardo Pastor
+// @author       oyekeno
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
